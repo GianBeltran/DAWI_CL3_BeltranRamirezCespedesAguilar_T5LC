d2b99a3229aa7996347e567f2e9d0e4459f5cffb
y4zper <78707317+y4zper@users.noreply.github.com>
2023-06-23 12:06:30 -0500

    service package
    UsuarioDetalleService
    UsuarioService

---
+++ src/main/java/pe/com/cibertec/DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC/service/UsuarioDetalleService.java
@@ -0,0 +1,62 @@
+package com.pe.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC.service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.pe.cibertec.appcinecibertec.model.bd.Usuario;
+import com.pe.cibertec.appcinecibertec.model.bd.Rol;
+
+@Service
+public class UsuarioDetalleService 
+	implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = 
+				usuarioService
+				.buscarUsuarioPorNomusuario(username);		
+		return usuarioPorAutenticacion(usuario, 
+				obtenerAutorizacionUsuario(usuario.getRoles()));
+	}
+	
+	private List<GrantedAuthority> 
+		obtenerAutorizacionUsuario(
+				Set<Rol> listRoles){
+		Set<GrantedAuthority> roles = 
+				new HashSet<GrantedAuthority>();
+		for(Rol rol: listRoles) {
+			roles.add(new 
+					SimpleGrantedAuthority(rol.getNomrol()));
+		}
+		List<GrantedAuthority> grantedAuthorities 
+			= new ArrayList<>(roles);
+		return grantedAuthorities;
+		
+		
+	}
+	
+	private UserDetails usuarioPorAutenticacion(
+			Usuario usuario,
+			List<GrantedAuthority> authorityList) {
+		return new User(usuario.getNomusuario(),
+				usuario.getPassword(),
+				usuario.getActivo(),
+				true, true, true, authorityList);
+	}
+
+}
---
+++ src/main/java/pe/com/cibertec/DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC/service/UsuarioService.java
@@ -0,0 +1,46 @@
+package com.pe.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC.service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.pe.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC.model.bd.Rol;
+import com.pe.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC.model.bd.Usuario;
+import com.pe.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC.repository.RolRepository;
+import com.pe.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private RolRepository rolRepository;
+	
+	private BCryptPasswordEncoder 
+		bCryptPasswordEncoder = 
+			new BCryptPasswordEncoder();
+	
+	public Usuario buscarUsuarioPorNomusuario
+		(String nomUsuario) {
+		return usuarioRepository
+				.findByNomusuario(nomUsuario);
+	}
+	
+	public Usuario guardarUsuario(
+			Usuario usuario) {
+		usuario.setPassword(
+				bCryptPasswordEncoder
+					.encode(usuario.getPassword()));
+		usuario.setActivo(true);
+		Rol rol = rolRepository
+				.findByNomrol("ADMIN");
+		usuario.setRoles(new HashSet<Rol>(
+				Arrays.asList(rol)));
+		return usuarioRepository.save(usuario);
+	}
+
+}
