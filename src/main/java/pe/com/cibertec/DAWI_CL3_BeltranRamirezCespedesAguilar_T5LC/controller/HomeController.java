9246e2c7309c35d488bdbca054f7c7e4e8e67dc5
Meldrid Cespedes Rodriguez <102499872+Meldrid@users.noreply.github.com>
2023-06-23 11:52:42 -0500

    PAQUETE CONTROLLER

---
+++ src/main/java/pe/com/cibertec/DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.pe.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+}
---
+++ src/main/java/pe/com/cibertec/DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC/controller/SesionController.java
@@ -0,0 +1,39 @@
+package com.pe.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pe.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC.modelo.bd.Usuario;
+import com.pe.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC.service.UsuarioService;
+
+
+
+
+@Controller
+@RequestMapping("/sesion")
+public class SesionController {
+	@Autowired
+	private UsuarioService usuarioService;
+	@GetMapping("/login")
+	public String login() {
+		return "sesion/frmLogin";
+	}
+	@GetMapping("/registrar")
+	public String registrar() {
+		return "sesiob/frmRegistro";
+	}
+	@PostMapping("/guardarUsuario")
+	public String guardarUsuario(
+			@ModelAttribute Usuario usuario,
+			Model model) {
+		usuarioService.guardarUsuario(usuario);
+		model.addAttribute("registroCorrecto",
+				true);
+		return "sesion/frmRegistro";
+	}
+}
