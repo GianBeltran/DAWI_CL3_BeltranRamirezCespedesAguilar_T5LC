7efd70fa60feab2c2c3da5a478fc7c9f396bc00f
GianBeltran <Gbeltranl0506@gmail.com>
2023-06-23 11:47:47 -0500

    Creacion del proyecto

---
+++ src/main/java/pe/com/cibertec/DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC/DawiCl3BeltranRamirezCespedesAguilarT5LcApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3BeltranRamirezCespedesAguilarT5LcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3BeltranRamirezCespedesAguilarT5LcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC/DawiCl3BeltranRamirezCespedesAguilarT5LcApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.DAWI_CL3_BeltranRamirezCespedesAguilar_T5LC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3BeltranRamirezCespedesAguilarT5LcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
